0c42edd29c5dd1c3909fa82647b350eb2980ea32
Anibal Hugo <ani2bal@yahoo.com>
2022-12-05 15:38:57 -0500

    working

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+#Factory
 def create_app():
     app = Flask(__name__)
 
@@ -7,4 +8,12 @@ def create_app():
     def hello():
         return 'Hello, PetFax'
 
+    #Register pet blueprint
+    from . import pet
+    app.register_blueprint(pet.bp)
+    
+    from . import fact 
+    app.register_blueprint(fact.bp)
+
+    #Return the app
     return app
